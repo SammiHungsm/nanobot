910bfe851b24e0cc51ae9386409dbc3db565277e
Sammi Hung <Sammi_Hung@kinetix.com.hk>
2026-04-25 10:13:12 +0100

    cleanup temp files

--- check_market_schema.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://postgres:postgres_password_change_me@postgres-financial:5432/annual_reports')
-cur = conn.cursor()
-cur.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'market_data' ORDER BY ordinal_position")
-for r in cur.fetchall():
-    print('%s: %s' % (r[0], r[1]))
-conn.close()
